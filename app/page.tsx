c28ca49c09f0862b694badfb5d61e4ebe88de5e2
sammit badodekar <badodekarsammit@gmail.com>
2023-12-22 08:31:30 +0530

    fix:status 500  in api/gmail

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import DisplayEmails from "@/components/ui/displayEmails";
 import { Button } from "@/components/ui/button";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
