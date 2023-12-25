d353ee4a40c237955fa8a8736b50568bd00897d2
Sai Vishwak <55258956+saivishwak@users.noreply.github.com>
2023-12-25 18:14:33 +0530

    use gpt-4

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -68,7 +68,7 @@ export const POST = async (req: NextRequest) => {
         emails.push(formattedEmail);
       });
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [
           {
             role: "system",
